8dc24ae6de20a26d2ceb9d825716255a8280fc99
kawa1228 <sxa.1105bo@gmail.com>
2018-09-28 15:37:32 +0900

    checkbox

--- src/scripts/entries/App.js
+++ src/scripts/entries/App.js
@@ -1,26 +1,12 @@
 import React from 'react'
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super()
     this.state = {
       area01: true,
-      numberOfGuests: 2
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
+    }
   }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
   render() {
     return (
       <form>
@@ -56,15 +42,6 @@ export default class App extends React.Component {
             checked={this.state.area04}
             onChange={this.handleInputChange} />
         </label>
-        <br />
-        {/* <label>
-          Number of guests:
-          <input
-            name="numberOfGuests"
-            type="number"
-            value={this.state.numberOfGuests}
-            onChange={this.handleInputChange} />
-        </label> */}
       </form>
     );
   }
